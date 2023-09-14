5c5f9ea9d23e42ec0019ba752917e96d88d58646
Gabriel Lantigua <143743234+glantig1@users.noreply.github.com>
2023-09-14 14:49:48 -0400

    I declared the properties to reference the views and assigned them to objects

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.SeekBar
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     // Declare view properties - the first one is done for you
     lateinit var displayTextView: TextView
+    lateinit var myButton: Button
+    lateinit var nameEditText : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +20,8 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
+        myButton = findViewById(R.id.clickMeButton)
+        nameEditText = findViewById(R.id.nameEditText)
 
         
         // Respond to button click event per specifications
